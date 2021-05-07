a51735a996033ffebe18cebb4731dd876ff6ccbb
prafalex <72709992+prafalex@users.noreply.github.com>
2021-05-07 21:49:13 +0300

    galerie animata

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const { restart } = require("nodemon");
 const fs=require("fs");
 const path=require('path');
 const sharp=require('sharp');
-
+const sassMiddleware= require('node-sass-middleware');
+const { json } = require("express");
+const { exec } = require("child_process");
 var app=express();
+const ejs=require('ejs');
 
 //set view
+app.set("view engine", "ejs");
 //functie numar random
 function numar() {
     var nr= Math.floor(Math.random() * (12 - 7) ) + 7;
@@ -45,8 +49,59 @@ function getSeason() {
     }
     return season
 }
-app.set("view engine", "ejs");
+
 app.set('trust proxy', true);
+
+app.get("*/gal-animata.css", function(req,res){
+    console.log('functie');
+    res.setHeader("Content-type","text-css");
+    let sCSS=fs.readFileSync("./resurse/scss/gal-animata.scss").toString("utf-8"); // citire scss
+    let rezsCSS=ejs.render(sCSS,{nr:numar});// transmit numarul catre scss
+    //console.log(rezsCSS);
+    fs.writeFileSync("./temp/gal-animata.scss",rezsCSS); // scriu scss in fisier temporar
+    exec("sass ./temp/gal-animata.scss ./temp/gal-animata.css", (error,stdout,stderr)=>{
+        if(error)
+        {
+            console.log(`error: ${error.message}`);
+            res.end();
+            return;
+        }
+        if(stderr)
+        {
+            console.log(`stderr: ${stderr}`);
+            res.end();
+            return;
+
+        }
+        
+        res.sendFile(path.join(__dirname,"temp/gal-animata.css"));
+        
+    })
+    });
+
+app.get("*/galerie-animata.css",function(req, res){
+    console.log("functie");
+    res.setHeader("Content-Type","text/css");//pregatesc raspunsul de tip css
+    let sirScss=fs.readFileSync("./resurse/scss/galerie-animata.scss").toString("utf-8");//citesc scss-ul cs string
+    let numar=imgrand.length;
+    console.log(numar);
+    let rezScss=ejs.render(sirScss,{nr:numar});// transmit culoarea catre scss si obtin sirul cu scss-ul compilat
+    fs.writeFileSync("./temp/galerie-animata.scss",rezScss);//scriu scss-ul intr-un fisier temporar
+    exec("sass ./temp/galerie-animata.scss ./temp/galerie-animata.css", (error, stdout, stderr) => {//execut comanda sass (asa cum am executa in cmd sau PowerShell)
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.end();
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            res.end();
+            return;
+        }
+        res.sendFile(path.join(__dirname,"temp/galerie-animata.css"));
+    });
+
+});
 
 app.get('/resurse/json/galerie.json',function(req,res){
     res.status(403).render("pagini/403");
@@ -54,12 +109,14 @@ app.get('/resurse/json/galerie.json',function(req,res){
 app.use("/resurse", express.static(__dirname+"/resurse"));
 
 function verificaImagini(){
+    var i=0;
     var anotimp=getSeason();
     var textFisier=fs.readFileSync("resurse/json/galerie.json");
     var jsi=JSON.parse(textFisier);
     var caleGalerie=jsi.cale_galerie;
     let vectorCai=[];
     for(let im of jsi.imagini){
+        if(i<12){
             if(anotimp==im.anotimp){
         var imVeche=path.join(caleGalerie,im.cale_fisier)
         var ext=path.extname(im.cale_fisier);
@@ -69,7 +126,11 @@ function verificaImagini(){
             .resize({ width: 250 })
             .toFile(imNoua);
         vectorCai.push({mare:imVeche,mic:imNoua,titlu:im.titlu});
+        i++;
             }
+        }
+        
+            
 }
 return vectorCai;
 };
@@ -90,6 +151,24 @@ function Imagini(){
 }
 return vectorCai;
 }
+function ImaginiEchipa(){
+    var textFisier=fs.readFileSync("resurse/json/echipa.json");
+    var jsi=JSON.parse(textFisier);
+    var caleGalerie=jsi.cale_galerie;
+    let vectorCai=[];
+    for(let im of jsi.imagini){
+        var imVeche=path.join(caleGalerie,im.cale_fisier)
+        var ext=path.extname(im.cale_fisier);
+        var numeFisier=path.basename(im.cale_fisier,ext);
+        var imNoua=path.join(caleGalerie+"/mic", numeFisier+"-mic"+".webp");
+        sharp(imVeche)
+            .resize({ width: 250 })
+            .toFile(imNoua);
+        vectorCai.push({mare:imVeche,mic:imNoua,titlu:im.titlu});
+}
+return vectorCai;
+}
+
 function ImaginiRand(){
     var textFisier=fs.readFileSync("resurse/json/galerie.json");
     var jsi=JSON.parse(textFisier);
@@ -110,31 +189,33 @@ function ImaginiRand(){
     }
     return vectorCai;
 }
+var imgrand=ImaginiRand();
 
-v=ImaginiRand();
-console.log(v.length);
-console.log(v);
 
-app.get("/",function(req,res){
-    let nr=numar();
-    console.log(nr);
+app.get(["/","/index"],function(req,res){
     let vectorcai=verificaImagini();
     res.render('pagini/index',{ip:req.ip,imagini:vectorcai});
    
 });
-app.get("/index",function(req,res){
-    let vectorcai=verificaImagini();
-    res.render('pagini/index',{ip:req.ip,imagini:vectorcai});
-   
-});
+
+
 
 app.get("/galerie",function(req,res){
     let vectorcai=Imagini();
-    let imgrand=ImaginiRand();
+    imgrand=ImaginiRand();
+    //console.log(imgrand.length);
     res.render('pagini/galerie',{imagini:vectorcai,rand:imgrand});
    
 });
 
+
+    
+app.get("/about",function(req,res)
+{
+    let echipa=ImaginiEchipa();
+    res.render('pagini/about',{echipa:echipa});
+    
+})
 
 app.get('/*', (req,res)=>{
     res.render("pagini"+req.url, (err,rezultatRender)=>{
@@ -149,5 +230,5 @@ app.get('/*', (req,res)=>{
             res.send(rezultatRender);    
     });
 })
-console.log("Hello world!");
-app.listen(8080, ()=> console.info("Listening on port 8080"));
+
+app.listen(8080, ()=> console.info("Hello! Listening on port 8080"));
